b309bd30be48fe584142726e0dfa2366f7dd4823
GAYATHRI G <gayathrigaya3ece@gmail.com>
2025-09-23 14:48:51 +0530

    first commit

---
+++ tnsif_java_gayathri/src/basics/java/BasicJava.java
@@ -0,0 +1,20 @@
+/*
+ All are considered or converted to object in java so it is an oopss language
+ Primitive data type are not objects so except primitive data type all are object
+ Objects are created inside main method
+ () ->method
+ new -> keyword
+ */
+package basics.java; //The package name should be same as the package which contain this basicjava class error will through if the another package name is given like package javating
+//import statement is present
+public class BasicJava {
+//declaration of variable
+	String name;
+	int age;
+	public static void main(String[] args) {
+		BasicJava obj = new BasicJava(); //class_name obj = reference_class_name();reference_class_name is a name of another class if we need to connect the two class
+		
+		
+	}
+
+}
